1052e1144a83b61ee37396f1b4ef670e471ab0b6
unknown <joaosilva2505@outlook.com>
2023-05-22 16:56:16 -0300

    (second commit)

--- code.js
+++ code.js
@@ -1,13 +1,33 @@
-const display= document.querySelector(".watch");
-var begin=0;
+const display = document.querySelector(".watch");
+const init = document.querySelector(".play");
+var sec = 0;
+var min = 0;
+var start;
+var minformatted = min.toString().padStart(2,'0')
+var secformatted = sec.toString().padStart(2, '0');
 
-const start= setInterval(function clock() {
-        begin++;
-        display.innerHTML=`<h2>${begin}</h2>`;
-    },1000)
+function iniciar() {
+  start = setInterval(function clock() {
+    sec++;
+    secformatted = sec.toString().padStart(2, '0');
+    display.innerHTML = `<h2>${min}:${secformatted}</h2>`;
 
-
+    if (sec >= 60) {
+      min++;
+      sec = 0;
+      secformatted = sec.toString().padStart(2, '0');
+      display.innerHTML = `<h2>${min}:${secformatted}</h2>`;
+    }
+  }, 100);
+}
 
 function restart() {
-    begin=-1;
+  sec = -1;
+  min = 0;
+  secformatted = sec.toString().padStart(2, '0');
+  minformatted= min.toString().padStart(2,'0');
+}
+
+function dispar() {
+  clearInterval(start);
 }
